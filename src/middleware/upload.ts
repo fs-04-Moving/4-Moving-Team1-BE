04113bcca7ba0c56372c3c1811ce99fe97440ab2
sungmin <usm0259@kaist.ac.kr>
2025-05-20 13:51:32 +0900

    feat:이미지관련 문제 해결

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,8 +1,12 @@
+import path from "path";
 import multer, { StorageEngine } from "multer";
 
 const storage: StorageEngine = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./upload");
+    // __dirname: /home/ubuntu/4-Moving-Team1-BE/dist/controllers (예시)
+    // ../upload → /home/ubuntu/4-Moving-Team1-BE/upload
+    const uploadPath = path.resolve(__dirname, "../upload");
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const extension = file.originalname.split(".").slice(-1)[0];
